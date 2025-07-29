fcfd08ac122755333d05ec8cc5a92e25c48f3735
Ziphyrien <Hanson132567@outlook.com>
2025-07-29 17:19:04 +0800

    更新站点图标

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -21,7 +21,8 @@ export default defineUserConfig({
 
   head: [
     // 配置站点图标
-    ['link', { rel: 'icon', type: 'image/png', href: 'https://theme-plume.vuejs.press/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
   ],
 
   bundler: viteBundler(),
